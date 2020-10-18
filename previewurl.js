11755d118d93d7a1c97c4c508ee8ebe63d1d2d0c
Ali <alimirz@gatech.edu>
2020-10-17 21:26:15 -0400

    all frontend added

---
+++ previewurl.js
@@ -0,0 +1,46 @@
+// const linkPreviewGenerator = require("link-preview-generator");
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// var path = require('path');
+// app.use(express.static('css'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+
+// async function preview(url) {
+// const previewData = await linkPreviewGenerator(
+//   url
+// );
+// console.log(previewData);
+
+
+
+// document.getElementById("gridd").innerHTML = "Paragraph changed.";
+// function myFunction() {
+//   document.getElementById("mom").innerHTML = "Paragraph changed.";
+// }
+
+
+// }
+// async function makeBlock() {
+// 	for(let i = 0; i < 3; i++) {
+// 		await preview("https://www.youtube.com/watch?v=8mqqY2Ji7_g")
+// 	}
+// 	// console.log(document)
+// 	// var para = document.createElement("p");
+// 	// var node = document.createTextNode("This is new.");
+// 	// para.appendChild(node);
+// 	// var element = document.getElementById("gridd");
+// 	// element.appendChild(para);
+
+// }
+// makeBlock()
+
